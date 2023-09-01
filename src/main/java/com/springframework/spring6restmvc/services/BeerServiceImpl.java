c29fbb59b76afcf098c4b63f9237c2d974879e7c
nthY <n_hat.yalcin@outlook.com>
2023-09-01 17:04:55 +0300

    Use Sprint tools and handle POST mappings for BeerController

--- src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -73,4 +73,25 @@ public class BeerServiceImpl implements BeerService {
 
         return beerMap.get(id);
     }
+
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .version(beer.getVersion())
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .build();
+
+        beerMap.put(savedBeer.getId(), savedBeer);
+
+        return savedBeer;
+    }
+
 }
