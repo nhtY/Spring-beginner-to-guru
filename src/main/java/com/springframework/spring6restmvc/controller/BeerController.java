e266dded07ce0ec9bdab9e5104ef021e7f486e55
nthY <n_hat.yalcin@outlook.com>
2023-09-01 14:57:54 +0300

    Adjust the BeerService and use @RestController to have a restfull api

--- src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
@@ -4,13 +4,15 @@ import com.springframework.spring6restmvc.model.Beer;
 import com.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
@@ -19,6 +21,11 @@ public class BeerController {
 //    public BeerController(BeerService beerService) {
 //        this.beerService = beerService;
 //    }
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
 
