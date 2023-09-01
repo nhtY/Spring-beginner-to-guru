3f0bfb8325a153c11f46d6c85b54d02999a75267
nthY <n_hat.yalcin@outlook.com>
2023-09-01 14:25:25 +0300

    Use @Slf4j of Lombok for logging. Set logging level to debug to see debug logs.

--- src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
@@ -1,9 +1,14 @@
 package com.springframework.spring6restmvc.controller;
 
+import com.springframework.spring6restmvc.model.Beer;
 import com.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
@@ -15,5 +20,11 @@ public class BeerController {
 //        this.beerService = beerService;
 //    }
 
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id - in controller...");
+
+        return beerService.getBeerById(id);
+    }
 
 }
--- src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,16 +2,21 @@ package com.springframework.spring6restmvc.services;
 
 import com.springframework.spring6restmvc.model.Beer;
 import com.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id in service was called.");
+
         return Beer.builder()
                 .id(id)
                 .version(1)
