ab0dbad25d5c697199c01e7011a6f052e5a27f4a
nthY <n_hat.yalcin@outlook.com>
2023-09-02 18:15:08 +0300

    use @RequiredArgsConstructor to allow constructor injection - not @AllArgsConstructor

--- src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/springframework/spring6restmvc/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.springframework.spring6restmvc.controller;
 import com.springframework.spring6restmvc.model.Beer;
 import com.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer") // BASE path for avery mapping in this class
 public class BeerController {
