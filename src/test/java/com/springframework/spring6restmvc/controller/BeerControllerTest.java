21655860252e648fa6bc2424b12deb8aee1c1582
nthY <n_hat.yalcin@outlook.com>
2023-09-02 21:47:48 +0300

    Use jsonPath to check returned json result has expected values for GET beer by id.

--- src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -10,13 +10,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.UUID;
 
 //@SpringBootTest we are not testing full application (we do not need all the beans, application context, etc.)
 @WebMvcTest(BeerController.class) // we are just testing BeerController (if not specified all controllers are generated)
@@ -40,16 +38,21 @@ class BeerControllerTest {
         // get the first beer in the list to use as a test object
         Beer testBeer = beerServiceImpl.listBeers().get(0);
 
-        // for any UUID if method returns a beer, then mockito will return the testBeer.
-        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+        // if method returns a beer, then mockito will return the testBeer.
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
         // HTTP GET : .../api/v1/beer/{UUID}
         // write 'Accept' header into the request.
         // Then, expect that the controller returns with HTTP 200 OK.
         // Then, check if there is a result as content and if its type is JSON
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+        // Then, check if returned beer id = test beer id
+        // Then, check if returned beer name = test beer name
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // for jsonPath usage: https://github.com/json-path/JsonPath
+                .andExpect(jsonPath("$.id", is(testBeer.getId())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 }
