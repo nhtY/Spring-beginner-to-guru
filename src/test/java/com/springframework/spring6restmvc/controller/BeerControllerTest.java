fad898898e49f829440bec54f301d38bbf7d84ec
nthY <n_hat.yalcin@outlook.com>
2023-09-03 17:37:42 +0300

    test deleteById() of BeerController. Use ArgumentCaptor to capture the arguments passed to related method of the mock service.

--- src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -6,6 +6,7 @@ import com.springframework.spring6restmvc.services.BeerService;
 import com.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,10 +15,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -46,6 +50,26 @@ class BeerControllerTest {
     @BeforeEach // before each test method, run the following
     void setUp() {
         beerServiceImpl = new BeerServiceImpl(); // each test method will have separate beerServiceImpl
+    }
+
+
+    @Test
+    void deleteById() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        mockMvc.perform(delete("/api/v1/beer/" + testBeer.getId().toString())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        // captures arguments for which method it is used
+        ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+
+        // verify that our mock beerService's deleteById() method is called.
+        // At the same time, capture the arguments passed to deleteById()
+        verify(beerService).deleteById(uuidArgumentCaptor.capture());
+
+        // Make sure that passed argument is the same as we passed when performing HTTP PUT above.
+        assertThat(testBeer.getId().equals(uuidArgumentCaptor.getValue()));
     }
 
     @Test
