805587428125563d88428ed8497554dc09775d8e
nthY <n_hat.yalcin@outlook.com>
2023-09-05 09:17:29 +0300

    Create JPA implementations of services and make them primary to use.

---
+++ src/main/java/com/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,50 @@
+package com.springframework.spring6restmvc.services;
+
+import com.springframework.spring6restmvc.mappers.BeerMapper;
+import com.springframework.spring6restmvc.model.BeerDTO;
+import com.springframework.spring6restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary // this implementation of the BeerService will be primarily used by spring
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void updateById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
---
+++ src/main/java/com/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,50 @@
+package com.springframework.spring6restmvc.services;
+
+import com.springframework.spring6restmvc.mappers.CustomerMapper;
+import com.springframework.spring6restmvc.model.CustomerDTO;
+import com.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary // this implementation of the CustomerService will be used primarily by Spring
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO createNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
