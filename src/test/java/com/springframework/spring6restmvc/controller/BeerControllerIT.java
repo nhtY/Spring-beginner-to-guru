f7e2775b72ecd00435d8b6690a78d712fcb00408
nthY <n_hat.yalcin@outlook.com>
2023-09-05 10:25:06 +0300

    Integration Test: check what happens if there is no record when listBeer() of controller invoked.

--- src/test/java/com/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -5,6 +5,8 @@ import com.springframework.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,5 +28,20 @@ class BeerControllerIT {
         List<BeerDTO> dtos = beerController.listBeers();
 
         assertThat(dtos.size()).isEqualTo(3); // we know that CommandLineRunner will create and save 3 beers.
+    }
+
+
+    // jpa test do rollback automatically, but we are using controller layer's methods here
+    @Rollback // rollback after test passed or failed.
+    @Transactional // this is required to be able to roll back
+    @Test
+    void testEmptyBeerList() {
+        // test if controller returns empty list, (not null, just empty list)
+
+        beerRepository.deleteAll(); // this will affect other test. So after the test, a rollback can reset the state
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos).isNotNull();
+        assertThat(dtos.size()).isEqualTo(0);
     }
 }
