c2cc07217212b7fa0a046f7b2c619bafd967b19c
nthY <n_hat.yalcin@outlook.com>
2023-09-20 15:52:52 +0300

    add a new method. Spring Data JPA would generate implementation based on the method name.

--- src/main/java/com/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/com/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -3,8 +3,11 @@ package com.springframework.spring6restmvc.repositories;
 import com.springframework.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 // CrudRepository could be extended, but JpaRepository already extends it. Besides Jpa repository provides jpa-specific methods: flushing JPA session etc.
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
