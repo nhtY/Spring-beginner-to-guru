767c3bc8225fef3d3b04b6dc113fbc5fb359b548
nthY <n_hat.yalcin@outlook.com>
2023-09-20 16:04:32 +0300

    Test Spring's auto generated JPA implementation method.

--- src/test/java/com/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,22 +1,36 @@
 package com.springframework.spring6restmvc.repositories;
 
+import com.springframework.spring6restmvc.bootstrap.BootstrapData;
 import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.model.BeerDTO;
 import com.springframework.spring6restmvc.model.BeerStyle;
+import com.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class}) // we need to first generate data, then test if we can list them
 class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testListBeerByNameIsLikeIgnoreCase() {
+        // %IPA% :  if it contains anywhere in the name
+        List<Beer> beers = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(beers.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeerWithTooLongUpc() {
