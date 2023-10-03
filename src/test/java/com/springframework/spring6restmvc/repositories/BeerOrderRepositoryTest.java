3263d6f12735b0ae7431589eb569c061097df887
nthY <n_hat.yalcin@outlook.com>
2023-10-03 18:08:17 +0300

    test the persistence of beer orders

--- src/test/java/com/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.springframework.spring6restmvc.repositories;
 
 import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.entities.BeerOrder;
 import com.springframework.spring6restmvc.entities.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @SpringBootTest
@@ -31,13 +33,22 @@ class BeerOrderRepositoryTest {
         testBeer = beerRepository.findAll().get(0);
     }
 
+    // about lazy initialization: https://www.linkedin.com/pulse/dissecting-hibernate-lazyinitializationexception-cause-jonas-bockstal/
+    @Transactional // used due to hibernate lazy initialization exception
     @Test
     void testBeerOrders() {
-      log.debug("total beer orders: {}", beerOrderRepository.count());
-      log.debug("total customers: {}", customerRepository.count());
-      log.debug("total beer count: {}", beerRepository.count());
-      log.debug("test customer: {}", testCustomer.getName());
-      log.debug("test beer: {}", testBeer.getBeerName());
+        BeerOrder beerOrder = BeerOrder.builder()
+              .customerRef("Test order")
+              .customer(testCustomer)
+              .build();
+
+      // BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+
+        // saveAndFlush method saves it immediately. In this way, a bidirectional relation can be established.
+        // Otherwise, order has customer but customer cannot see this order in its orders list.
+        BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+
+        log.debug("saved order: {}", savedBeerOrder.getCustomerRef());
 
     }
 }
